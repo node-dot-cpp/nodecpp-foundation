9e3c1e60da03bef24abe1efdb5841e7907382eee
dmytro.ivanchykhin <di@o-log-n.com>
2018-12-10 16:45:41 +0200

    adding cpu exception translation means

---
+++ src/cpu_exceptions_translator.cpp
@@ -0,0 +1,119 @@
+/* -------------------------------------------------------------------------------
+* Copyright (c) 2018, OLogN Technologies AG
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the <organization> nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* -------------------------------------------------------------------------------*/
+
+
+#include "../include/cpu_exceptions_translator.h"
+
+#if defined __GNUC__ && __linux__
+
+#include <signal.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <execinfo.h>
+
+#define STRINGIFY_VALUE_OF_HELPER(x) #x
+#define STRINGIFY_VALUE_OF(x) STRINGIFY_VALUE_OF_HELPER(x)
+
+struct kernel_sigaction // see http://man7.org/linux/man-pages/man2/sigaction.2.html
+{
+	void (*k_sa_sigaction)(int,siginfo_t *,void *);
+	unsigned long k_sa_flags;
+	void (*k_sa_restorer) (void);
+	sigset_t k_sa_mask;
+};
+
+#ifdef __x86_64__
+
+__asm__
+  (
+   ".align 16\n"
+   "sigRestorerAsm: movq $" STRINGIFY_VALUE_OF(__NR_rt_sigreturn) ", %rax\n"
+   "syscall\n"
+   );
+   
+void sigRestorer () __asm__ ("sigRestorerAsm");
+
+#else // __x86_64__
+
+#error
+
+#endif // __x86_64__
+
+static void unblockSignal(int signum)
+{
+    sigset_t sigs;
+    sigemptyset(&sigs);
+    sigaddset(&sigs, signum);
+    sigprocmask(SIG_UNBLOCK, &sigs, NULL);
+}
+
+static void sigAction(int, siginfo_t* siginfo, void*)
+{
+    unblockSignal(SIGSEGV);    
+    if( (uintptr_t)(siginfo->si_addr) >= NODECPP_GUARD_PAGE_SIZE )
+		throw MemoryAccessViolationException( siginfo->si_addr );
+	else 
+		throw NullPointerException();
+}
+
+void initTranslator()
+{
+    struct kernel_sigaction ks;
+    ks.k_sa_sigaction = sigAction;
+    sigemptyset (&ks.k_sa_mask);
+    ks.k_sa_flags = SA_SIGINFO|0x4000000;
+    ks.k_sa_restorer = sigRestorer;
+    syscall (SYS_rt_sigaction, SIGSEGV, &ks, NULL, _NSIG / 8);
+}
+
+#else // assuming windows; // TODO: check explicitly
+
+#include <stdio.h>
+#include <windows.h>
+#include <eh.h>
+
+void trans_func(unsigned int u, EXCEPTION_POINTERS* ep)
+{
+    if (ep->ExceptionRecord->ExceptionCode == EXCEPTION_ACCESS_VIOLATION && ep->ExceptionRecord->NumberParameters >= 2 )
+	{
+		void * at = (void*)(ep->ExceptionRecord->ExceptionInformation[1]);
+		if ( (uintptr_t)at >= NODECPP_GUARD_PAGE_SIZE )
+			throw MemoryAccessViolationException(at);
+		else 
+			throw NullPointerException();
+	}
+	else
+		throw std::exception();
+}
+
+void initTranslator()
+{
+    _set_se_translator(trans_func);
+}
+
+
+#endif // __GNUC__ && __linux
---
+++ test/test_seh.cpp
@@ -0,0 +1,130 @@
+/* -------------------------------------------------------------------------------
+* Copyright (c) 2018, OLogN Technologies AG
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the <organization> nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* -------------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdexcept>
+#include <string>
+
+#include "../include/cpu_exceptions_translator.h"
+
+using namespace std;
+
+class TestBase
+{
+	int n1;
+public:
+	TestBase( int k ) { n1 = k; printf( "TestBase::TestBase(%d)\n", k ); }
+	virtual ~TestBase() { printf( "TestBase::~TestBase()\n" ); }
+
+};
+
+class Test : public TestBase
+{
+	int n2;
+public:
+	Test( int k ) : TestBase(k+1) { n2 = k;  printf( "Test::Test(%d)\n", k ); }
+	virtual ~Test() { printf( "Test::~Test()\n" ); }
+
+};
+
+void badCallInner_Nullptr()
+{
+	volatile int * ptr = nullptr;
+    *ptr = 0;
+}
+
+void badCallOuter_Nullptr()
+{
+	TestBase tb( 1 );
+	badCallInner_Nullptr();
+	printf( "we must be dead here\n" );
+}
+
+void badCallInner_AnyPtr()
+{
+	volatile int * ptr = (int*) 0x100000;
+    *ptr = 0;
+}
+
+void badCallOuter_AnyPtr()
+{
+	TestBase tb( 10 );
+	badCallInner_AnyPtr();
+	printf( "we must be dead here\n" );
+}
+
+void badCallInner_DivByZero()
+{
+	volatile int a = 0;
+	volatile int b = 3 / a;
+	printf( "%d\n", b ); // dummy call to avoid optimizing out
+}
+
+void badCallOuter_DivByZero()
+{
+	TestBase tb( 100 );
+	badCallInner_DivByZero();
+	printf( "we must be dead here\n" );
+}
+
+void testSEH()
+{
+    initTranslator();
+
+    try
+    {
+		badCallOuter_Nullptr();
+    }
+    catch (std::exception& e)
+    {
+        printf( "%s\n", e.what() );
+    }
+
+    printf( "... and still working [1]\n" );
+
+    try
+    {
+		badCallOuter_AnyPtr();
+	}
+    catch (std::exception& e)
+    {
+        printf( "%s\n", e.what() );
+    }
+
+    printf( "... and still working [2]\n" );
+	
+    try 
+	{
+		badCallOuter_DivByZero();
+	}
+	catch( std::exception& e )
+	{
+		printf( "%s\n", e.what() );
+	}
+
+    printf( "... and still working [3]\n" );
+}

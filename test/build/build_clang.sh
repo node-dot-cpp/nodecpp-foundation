cb31133752a568f3bf07700ba6a05beca547af6e
dmytro.ivanchykhin <di@o-log-n.com>
2018-12-18 19:00:01 +0200

    updated build scripts; looks to be working

--- test/build/build_clang.sh
+++ test/build/build_clang.sh
@@ -1,2 +1,2 @@
 rm ./test.bin 
-clang++ ../main.cpp ../test_seh.cpp ../../src/cpu_exceptions_translator.cpp -Wall -fexceptions -fnon-call-exceptions -std=c++17 -o test.bin
+clang++ ../main.cpp ../test_seh.cpp ../../src/log.cpp ../../3rdparty/fmt/src/format.cc ../../src/cpu_exceptions_translator.cpp -I../../3rdparty/fmt/include -DNODECPP_CUSTOM_LOG_PROCESSING="\"../test/my_logger.h\"" -Wall -fexceptions -fnon-call-exceptions -std=c++17 -o test.bin
--- test/build/build_gcc.sh
+++ test/build/build_gcc.sh
@@ -1,2 +1,2 @@
 rm ./test.bin
-g++ ../main.cpp ../test_seh.cpp ../../src/cpu_exceptions_translator.cpp -std=c++17 -Wall -fexceptions -fnon-call-exceptions -o test.bin
+g++ ../main.cpp ../test_seh.cpp ../../src/log.cpp ../../3rdparty/fmt/src/format.cc ../../src/cpu_exceptions_translator.cpp -I../../3rdparty/fmt/include -DNODECPP_CUSTOM_LOG_PROCESSING="\"../test/my_logger.h\"" -std=c++17 -Wall -fexceptions -fnon-call-exceptions -o test.bin

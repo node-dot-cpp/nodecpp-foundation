d27a171bbdd9b1db19da48b30ff4c835de2efae3
dmytro.ivanchykhin <di@o-log-n.com>
2018-12-20 21:26:50 +0200

    adding assertion means (customization)

--- include/default_assert.h
+++ include/default_assert.h
@@ -25,56 +25,15 @@
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 * -------------------------------------------------------------------------------*/
 
-#ifndef NODECPP_ASSERT_H
-#define NODECPP_ASSERT_H 
+#ifndef NODECPP_DEFAULT_ASSERT_H
+#define NODECPP_DEFAULT_ASSERT_H 
 
 #include "platform_base.h"
-#include "../3rdparty/fmt/include/fmt/format.h"
-#include "log.h"
 
+template< ModuleIDType module, AssertLevel level>
+struct ShouldAssert
+{
+	static constexpr ActionType value = ActionType::throwing;
+};
 
-namespace nodecpp::assert { // pedantic regular critical
-
-	enum class AssertLevel { critical = 0, regular = 1, pedantic = 2 }; // just a sketch; TODO: revise
-	using ModuleIDType = int; // at least, so far
-
-	enum class ActionType { ignoring, throwing, terminating };
-
-#ifdef NODECPP_CUSTOM_ASSERT_PROCESSING
-#include NODECPP_CUSTOM_ASSERT_PROCESSING
-#else
-#include "default_assert.h"
-#endif // NODECPP_CUSTOM_LOG_PROCESSING
-
-	template< ModuleIDType module, AssertLevel level, class Expr, typename... ARGS>
-	void nodecpp_assert( const char* file, int line, const Expr& expr, const char* condString, const char* formatStr, const ARGS& ... args ) {
-		if constexpr ( ShouldAssert<module, level>::value != ActionType::ignoring )
-		{
-			if ( expr() )
-				return;
-			constexpr size_t logBufSz = 1024;
-			char logBuf[logBufSz];
-			auto res = fmt::format_to_n( logBuf, logBufSz-1, formatStr, args... );
-			if ( res.size >= logBufSz )
-			{
-				logBuf[logBufSz-4] = '.';
-				logBuf[logBufSz-3] = '.';
-				logBuf[logBufSz-2] = '.';
-				logBuf[logBufSz-1] = 0;
-			}
-			else
-				logBuf[res.size] = 0;
-			nodecpp::log::log<module, nodecpp::log::LogLevel::error>("File \"{}\", line {}: assertion \'{}\' failed, message: \"{}\"", file, line, condString, logBuf );
-			if constexpr ( ShouldAssert<module, level>::value == ActionType::throwing )
-				throw std::exception();
-			else
-				std::abort();
-		}
-	}
-
-#define NODECPP_ASSERT( module, level, condition, ... ) \
-	nodecpp::assert::nodecpp_assert<module, level>( __FILE__, __LINE__, [&] { return condition; }, #condition, __VA_ARGS__ )
-
-} // namespace nodecpp::assert
-
-#endif // NODECPP_ASSERT_H
+#endif // NODECPP_DEFAULT_ASSERT_H

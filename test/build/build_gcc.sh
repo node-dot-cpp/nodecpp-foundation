3c5bc12a9869d2385aa3f46e582356ecf939ea51
dmytro.ivanchykhin <di@o-log-n.com>
2018-12-28 17:32:50 +0200

    updating build scripts for clang and gcc

--- test/build/build_gcc.sh
+++ test/build/build_gcc.sh
@@ -1,2 +1,2 @@
-rm ./test.bin
-g++ ../main.cpp ../test_seh.cpp ../../src/log.cpp ../../3rdparty/fmt/src/format.cc ../../src/cpu_exceptions_translator.cpp -I../../3rdparty/fmt/include -DNODECPP_CUSTOM_LOG_PROCESSING="\"../test/my_logger.h\"" -std=c++17 -Wall -fexceptions -fnon-call-exceptions -o test.bin
+rm ./test.bin 
+g++ ../main.cpp ../test_seh.cpp ../../3rdparty/fmt/src/format.cc ../../src/log.cpp ../../src/cpu_exceptions_translator.cpp ../../src/std_error.cpp ../samples/file_error.cpp ../samples/safe_memory_error.cpp -I../../3rdparty/fmt/include -DNODECPP_CUSTOM_LOG_PROCESSING="\"../test/my_logger.h\"" -std=c++17 -Wall -fexceptions -fnon-call-exceptions -o test.bin 

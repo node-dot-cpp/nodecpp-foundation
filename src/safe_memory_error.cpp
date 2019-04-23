6e331c3b484f3e0f873ebc3b5e3eea1a61af9daa
dmytro.ivanchykhin <di@o-log-n.com>
2019-04-23 09:30:03 +0300

    fixing include path

--- src/safe_memory_error.cpp
+++ src/safe_memory_error.cpp
@@ -25,7 +25,7 @@
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 * -------------------------------------------------------------------------------*/
 
-#include "safe_memory_error.h"
+#include "../include/safe_memory_error.h"
 
 namespace nodecpp::error {
 

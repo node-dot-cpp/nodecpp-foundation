f26634c8919f28a618bd6071257d9551fe0e2ea0
Marcos Bracco <mb@o-log-n.com>
2020-06-17 17:29:22 -0300

    more travis

--- tools/CommonCppFlags.cmake
+++ tools/CommonCppFlags.cmake
@@ -17,7 +17,7 @@ if (MSVC)
   string( REPLACE "/EHsc" "/EHa" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" )
 
   add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-  add_definitions(-D_CRT_NONSTDC_NO_WARNINGS)
+#  add_definitions(-D_CRT_NONSTDC_NO_WARNINGS)
   
   add_compile_options( /W3 )
   add_compile_options( /wd4267 ) # conversion, possible loss of data
@@ -33,14 +33,14 @@ if (MSVC)
   endif()
 else ()
   	# if building release, use O2 instead of O3
-  string(REPLACE "-O3" "-O2" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+#  string(REPLACE "-O3" "-O2" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 
   add_compile_options( -Wall )
   add_compile_options( -Wextra )
   add_compile_options( -Wno-unused-variable )
   add_compile_options( -Wno-unused-parameter )
   add_compile_options( -Wno-empty-body )
-  add_compile_options( -fexceptions )
-  add_compile_options( -fnon-call-exceptions )
+#  add_compile_options( -fexceptions )
+#  add_compile_options( -fnon-call-exceptions )
 endif()
 

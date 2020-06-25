337edb427f37218c95d290433dbde4171a49c2b6
dmytro.ivanchykhin <di@o-log-n.com>
2020-06-25 12:58:37 +0300

    using llvm symbolize

--- src/stack_info.cpp
+++ src/stack_info.cpp
@@ -48,6 +48,108 @@
 #include <cxxabi.h>
 #endif // NODECPP_LINUX_NO_LIBUNWIND
 
+#ifdef NODECPP_STACKINFO_USE_LLVM_SYMBOLIZE
+
+#include "llvm/DebugInfo/Symbolize/Symbolize.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string>
+
+static char path2thisExe[0x400];
+static bool path2thisExeLoaded = false;
+static const char* path2me() {
+	if ( path2thisExeLoaded )
+		return path2thisExe;
+
+    int ret;
+
+    ret = readlink("/proc/self/exe", path2thisExe, sizeof(path2thisExe)-1);
+    if(ret ==-1)
+		return "";
+    path2thisExe[ret] = 0;
+	path2thisExeLoaded = true;
+printf( "Path of my EXE: %s\n\n", path2thisExe );
+    return path2thisExe;
+}
+
+/*static uintptr_t baseAddrOfthisExe = 0;
+static uintptr_t baseAddr()
+{
+	if ( baseAddrOfthisExe )
+		return baseAddrOfthisExe;
+
+    int ret;
+
+    ret = readlink("/proc/self/exe", path2thisExe, sizeof(path2thisExe)-1);
+    if(ret ==-1)
+		return "";
+    path2thisExe[ret] = 0;
+	path2thisExeLoaded = true;
+printf( "Path of my EXE: %s\n\n", path2thisExe );
+    return path2thisExe;
+}*/
+
+#ifndef _GNU_SOURCE
+#error _GNU_SOURCE must be defined to proceed
+#endif
+
+#include <dlfcn.h>
+
+static uintptr_t addr2offset( void* fnAddr ) {
+	Dl_info info;
+	int ret = dladdr(fnAddr, &info); // see https://linux.die.net/man/3/dlopen for details
+	if ( ret == 0 )
+		return 0;
+	return (uintptr_t)fnAddr - (uintptr_t)(info.dli_fbase);
+}
+
+using namespace llvm;
+using namespace symbolize;
+
+template<typename T>
+static bool error(Expected<T> &ResOrErr) {
+	if (ResOrErr)
+		return false;
+//	logAllUnhandledErrors(ResOrErr.takeError(), errs(), "LLVMSymbolizer: error reading file: ");
+	consumeError( ResOrErr.takeError() );
+	return true;
+}
+
+static void addFileLineInfo( const char* ModuleName, uintptr_t Offset, std::string& out, bool addFnName ) {
+	LLVMSymbolizer Symbolizer;
+
+	auto ResOrErr = Symbolizer.symbolizeInlinedCode( ModuleName, {Offset, object::SectionedAddress::UndefSection});
+
+	if ( !error(ResOrErr) )
+	{
+		auto& info = ResOrErr.get();
+		size_t numOfFrames = info.getNumberOfFrames();
+		if ( numOfFrames == 0 )
+			return;
+		const DILineInfo & li = info.getFrame(0);
+		if ( addFnName )
+			out += fmt::format( "at {} in {}, line {}:{}\n", li.FunctionName.c_str(), li.FileName.c_str(), li.Line, li.Column );
+		else
+			out += fmt::format( " in {}, line {}:{}\n", li.FileName.c_str(), li.Line, li.Column );
+
+		/*printf( "num of frames: %d\n", (int)(numOfFrames) );
+		for ( size_t i=0; i<numOfFrames; ++i )
+		{
+			const DILineInfo & li = info.getFrame(i);
+			printf( "  [%zd]\n", i );
+			printf( "    file: %s\n", li.FileName.c_str() );
+			printf( "    func: %s\n", li.FunctionName.c_str() );
+			printf( "    line: %d\n", li.Line );
+			printf( "    clmn: %d\n", li.Column );
+			printf( "    s.l.: %d\n", li.StartLine );
+		}*/
+	}
+}
+
+#endif // NODECPP_STACKINFO_USE_LLVM_SYMBOLIZE
+
 #else
 #error not (yet) supported
 #endif
@@ -105,7 +207,13 @@ namespace nodecpp {
 			std::string out;
 			for (int i = 0; i < numberOfFrames; i++)
 			{
+#ifdef NODECPP_STACKINFO_USE_LLVM_SYMBOLIZE
+				out += fmt::format( "\tat {}", btsymbols[i] );
+//				addFileLineInfo( path2me(), (uintptr_t)(stack[i]), out, true );
+				addFileLineInfo( path2me(), addr2offset(stack[i]), out, true );
+#else
 				out += fmt::format( "\tat {}\n", btsymbols[i] );
+#endif // NODECPP_STACKINFO_USE_LLVM_SYMBOLIZE
 			}
 			free( btsymbols );
 			if ( stripPoint != nullptr )
@@ -142,7 +250,13 @@ namespace nodecpp {
 				if (status == 0) {
 					nameptr = demangled;
 				}
+#ifdef NODECPP_STACKINFO_USE_LLVM_SYMBOLIZE
+				out += fmt::format( " ({}+0x{:x})", nameptr, offset );
+//				addFileLineInfo( path2me(), offset, out, false );
+				addFileLineInfo( path2me(), addr2offset(pc + offset), out, true );
+#else
 				out += fmt::format( " ({}+0x{:x})\n", nameptr, offset );
+#endif // NODECPP_STACKINFO_USE_LLVM_SYMBOLIZE
 				std::free(demangled);
 			} else {
 				out += "<...>\n";

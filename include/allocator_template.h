c2190147e1dbb668be4663e8c4d11d095c83aa4c
dmytro.ivanchykhin <di@o-log-n.com>
2020-08-04 13:14:26 +0300

    updating alignment-related defs (misc fixes)

--- include/allocator_template.h
+++ include/allocator_template.h
@@ -90,7 +90,7 @@ namespace nodecpp {
 
 		NODISCARD _Ty * allocate(const size_t _Count)
 		{
-			static_assert( std::alignment_of_v<_Ty> <= RawAllocT::guaranteed_alignment );
+			static_assert( alignof(_Ty) <= RawAllocT::guaranteed_alignment );
 			size_t iniByteSz = getByteSizeOfNElem(_Count);
 			constexpr size_t alignment = alignof(_Ty) > static_cast<size_t>(__STDCPP_DEFAULT_NEW_ALIGNMENT__) ? alignof(_Ty) : static_cast<size_t>(__STDCPP_DEFAULT_NEW_ALIGNMENT__);
 
@@ -105,7 +105,7 @@ namespace nodecpp {
 
 	struct StdRawAllocator
 	{
-		static constexpr size_t guaranteed_alignment = NODECPP_GUARANTEED_IIBMALLOC_ALIGNMENT;
+		static constexpr size_t guaranteed_alignment = NODECPP_GUARANTEED_MALLOC_ALIGNMENT;
 		static NODECPP_FORCEINLINE void* allocate( size_t allocSize ) { return ::malloc( allocSize ); }
 		static NODECPP_FORCEINLINE void* allocate( size_t allocSize, size_t allignment ) { return ::malloc( allocSize ); } // TODO: address alignment
 		static NODECPP_FORCEINLINE void deallocate( void* ptr) { ::free( ptr ); }

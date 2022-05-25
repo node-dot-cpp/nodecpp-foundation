227323b4ef9b931bfe32761efb2653185e613631
Marcos Bracco <mb@sweetdreams3d.com>
2022-05-25 16:38:14 -0300

    update ci script

--- tools/ci-android-r23b.sh
+++ tools/ci-android-r23b.sh
@@ -7,7 +7,7 @@ cd build/android-r23b
 
 # env variable ANDROID_NDK should point to the root folder where NDKs are installed
 
-cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-23 -DANDROID_NDK=${ANDROID_NDK}/android-ndk-r23b -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/android-ndk-r23b/build/cmake/android.toolchain.cmake ../..
+cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-23 -DANDROID_NDK=${ANDROID_NDK}/android-ndk-r23b -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/android-ndk-r23b/build/cmake/android.toolchain.cmake -G Ninja ../..
 
 cmake --build .
 
